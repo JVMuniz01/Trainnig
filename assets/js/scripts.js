2634c0be32e0ddcc12614b36f69e06fb2101a973
Muniz <j040vmr@gmail.com>
2023-07-09 08:57:54 -0300

    IMC

---
+++ assets/js/scripts.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const weigth = document.getElementById('weight').value;
+    const height = document.getElementById('height').value;
+
+    const bmi = (weigth / (height * height)).toFixed(2);
+
+    const value = document.getElementById('value');
+    let description = '';
+    document.getElementById('infos').classList.remove('hidden');
+
+
+    if (bmi < 18.5) {
+        description = 'Cuidado! Você está abaixo do peso';
+    } else if (bmi > 18.5 && bmi <= 25) {
+        description = 'Peso ideal!';
+    } else if (bmi > 25 && bmi <= 30) {
+        description = 'Peso ideal!';
+    }
+    value.textContent = bmi;
+    document.getElementById('descricption').textContent = description;
+});
